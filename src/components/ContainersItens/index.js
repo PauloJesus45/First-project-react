3dad437149cc69291dc8b61bd10a21f221ff1665
paulojesus45 <paulojesusnpp@gmail.com>
2023-12-04 15:39:11 -0300

    projeto React

---
+++ src/components/ContainersItens/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import { ContainerIntens as Container} from "./styles";
+
+function ContainerIntens({children,isBlur}){
+
+    return <Container isBlur={isBlur}>{children}</Container>
+}
+
+export default ContainerIntens
